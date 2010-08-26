00f0d586e61faf5e295f628b6d2761cc5a70d16d
mwaskom <mwaskom@mit.edu>
2010-08-26 14:36:28 -0400

    Adding report nodes to first level modules

--- fluid_fsl_fixed_fx.py
+++ fluid_fsl_fixed_fx.py
@@ -1,4 +1,3 @@
-
 import os
 import nipype.interfaces.io as nio
 import nipype.interfaces.fsl as fsl
@@ -41,12 +40,23 @@ Use :class:`nipype.interfaces.fsl.FLAMEO` to estimate a second level model
 flameo = pe.MapNode(interface=fsl.FLAMEO(run_mode='fe'), name="flameo",
                     iterfield=['cope_file','var_cope_file'])
 
+overlayflame = pe.MapNode(interface=fsl.Overlay(stat_thresh=(2.5, 10),
+                                                auto_thresh_bg=True,
+                                                show_negative_stats=True),
+                          name='overlayflame',
+                          iterfield = ['stat_image'])
+
+sliceflame = pe.MapNode(interface=fsl.Slicer(all_axial=True,
+                                             image_width=750),
+                        name='sliceflame',
+                        iterfield=['in_file'])
+
 fixed_fx.connect([(copemerge,flameo,[('merged_file','cope_file')]),
                   (varcopemerge,flameo,[('merged_file','var_cope_file')]),
                   (level2model,flameo, [('design_mat','design_file'),
                                         ('design_con','t_con_file'),
                                         ('design_grp','cov_split_file')]),
+                  (flameo,overlayflame,[('zstats','stat_image')]),
+                  (overlayflame,sliceflame,[('out_file', 'in_file')])
                   ])
-
-
 
--- fluid_fsl_model.py
+++ fluid_fsl_model.py
@@ -1,5 +1,6 @@
 import nipype.algorithms.modelgen as model
 import nipype.interfaces.fsl as fsl
+import nipype.interfaces.utility as util
 import nipype.pipeline.engine as pe
 
 
@@ -21,14 +22,48 @@ modelestimate = pe.MapNode(interface=fsl.FILMGLS(smooth_autocorr=True,
                            name='modelestimate',
                            iterfield = ['design_file','in_file'])
 
+threshres = pe.MapNode(interface=fsl.ImageStats(op_string = '-r'),
+                       name='threshresidual',
+                       iterfield=['in_file'])
+
+overlayres = pe.MapNode(interface=fsl.Overlay(auto_thresh_bg=True),
+                        name='overlayresidual',
+                        iterfield = ['stat_image', 'stat_thresh'])
+
+sliceres = pe.MapNode(interface=fsl.Slicer(all_axial=True,
+                                           image_width=750),
+                      name='sliceresidual',
+                      iterfield=['in_file'])
+
 contrastestimate = pe.MapNode(interface=fsl.ContrastMgr(), name='contrastestimate',
                          iterfield = ['tcon_file','stats_dir'])
+
+selectcontrast = pe.MapNode(interface=util.Select(), name='selectcontrast', iterfield=['inlist'])
+
+overlaystats = pe.MapNode(interface=fsl.Overlay(stat_thresh=(2.5,10),
+                                                auto_thresh_bg=True,
+                                                show_negative_stats=True),
+                         name='overlaystats',
+                         iterfield = ['stat_image'])
+                     
+slicestats = pe.MapNode(interface=fsl.Slicer(all_axial=True,
+                                             image_width=750),
+                        name='slicestats',
+                        iterfield=['in_file'])
+
 
 fsl_modelfit.connect([
    (modelspec,level1design,[('session_info','session_info')]),
    (level1design,modelgen,[('fsf_files','fsf_file')]),
    (modelgen,modelestimate,[('design_file','design_file')]),
    (modelgen,contrastestimate,[('con_file','tcon_file')]),
+   (modelestimate,threshres,[('sigmasquareds','in_file')]),
+   (modelestimate,overlayres,[('sigmasquareds','stat_image')]),
+   (threshres,overlayres,[(('out_stat', lambda x: [tuple(i) for i in x]), 'stat_thresh')]),
+   (overlayres,sliceres,[('out_file', 'in_file')]),
    (modelestimate,contrastestimate,[('results_dir','stats_dir')]),
+   (contrastestimate,selectcontrast,[('zstats','inlist')]),
+   (selectcontrast, overlaystats, [('out','stat_image')]),
+   (overlaystats, slicestats, [('out_file', 'in_file')]),
    ])
 
