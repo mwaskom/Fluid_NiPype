dd193b095399baf4f82b3a7c5ee250dfee188c1b
mwaskom <mwaskom@mit.edu>
2010-09-09 21:52:44 -0400

    Finished(?) Freesurfing the preproc module

--- fluid_preproc.py
+++ fluid_preproc.py
@@ -1,5 +1,3 @@
-# vi: set ft=python sts=4 ts=4 sw=4 et:
-# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 """
     Preprocessing module for Fluid Intelligence fMRI paradigms.
 """
@@ -15,72 +13,51 @@ import nipype.pipeline.engine as pe
 import nipype.algorithms.rapidart as ra      
 import nibabel
 
+
 preproc = pe.Workflow(name="preproc")
 
 """
 Set up a node to define all inputs required for the preprocessing workflow
 """
 
-inputnode = pe.Node(interface=util.IdentityInterface(fields=['func',
-                                                             'struct']),
-                    name='inputspec')
+inputnode = pe.Node(interface=util.IdentityInterface(fields=["func",
+                                                             "subject_id"]),
+                    name="inputspec")
 
 """
 Convert functional images to float representation. Since there can be more than
 one functional run we use a MapNode to convert each run.
 """
 
-img2float = pe.MapNode(interface=fsl.ImageMaths(out_data_type='float',
-                                             op_string = '',
-                                             suffix='_dtype'),
-                       iterfield=['in_file'],
-                       name='img2float')
-preproc.connect(inputnode, 'func', img2float, 'in_file')
+img2float = pe.MapNode(interface=fsl.ImageMaths(out_data_type="float",
+                                             op_string = "",
+                                             suffix="_dtype"),
+                       iterfield=["in_file"],
+                       name="img2float")
+preproc.connect(inputnode, "func", img2float, "in_file")
 
 """
 Extract the middle volume of the first run as the reference
 """
 
-extract_ref = pe.MapNode(interface=fsl.ExtractROI(t_size=1),
+extract_ref = pe.MapNode(interface=fsl.ExtractROI(t_size=1,
+                                                  t_min=0),
                          iterfield=["in_file"],
-                         name = 'extractref')
+                         name = "extractref")
+
+preproc.connect(img2float, "out_file", extract_ref, "in_file")
 
 """
-Define a function to pick the first file from a list of files
-"""
-
-def pickfirst(files):
-    if isinstance(files, list):
-        return files[0]
-    else:
-        return files
-
-preproc.connect(img2float, 'out_file', extract_ref, 'in_file')
-
-"""
-Define a function to return the 1 based index of the middle volume
-"""
-
-def getmiddlevolume(func):
-    funcfile = func
-    if isinstance(func, list):
-        funcfile = func[0]
-    _,_,_,timepoints = nibabel.load(funcfile).get_shape()
-    return (timepoints/2)-1
-
-preproc.connect(inputnode, ('func', getmiddlevolume), extract_ref, 't_min')
-
-"""
-Realign the functional runs to the middle volume of that run
+Realign each functional run to the middle volume of the run
 """
 
 motion_correct = pe.MapNode(interface=fsl.MCFLIRT(save_mats = True,
                                                   save_plots = True,
                                                   save_rms = True),
-                            name='realign',
-                            iterfield = ['in_file', "ref_file"])
-preproc.connect(img2float, 'out_file', motion_correct, 'in_file')
-preproc.connect(extract_ref, 'roi_file', motion_correct, 'ref_file')
+                            name="realign",
+                            iterfield = ["in_file", "ref_file"])
+preproc.connect(img2float, "out_file", motion_correct, "in_file")
+preproc.connect(extract_ref, "roi_file", motion_correct, "ref_file")
 
 """
 Plot the motion and displacement estimates from MCFLIRT
@@ -102,213 +79,257 @@ plotdisp = pe.MapNode(interface=fsl.PlotMotionParams(in_source="fsl",
 preproc.connect(motion_correct, "rms_files", plotdisp, "in_file")
 
 """
-Get a mean image for each realigned run
+Extract the mean volume of each functional run
 """
 
-meanfunc = pe.MapNode(interface=fsl.ImageMaths(op_string='-Tmean',
-                                               suffix='_mean'),
-                       iterfield=['in_file'],
-                       name='meanfunc')
-
-preproc.connect(realign, "out_file", meanfunc, "in_file")
+meanfunc = pe.MapNode(interface=fsl.ImageMaths(op_string = "-Tmean",
+                                               suffix="_mean"),
+                      iterfield=["in_file"],
+                      name="meanfunc")
+preproc.connect(motion_correct, "out_file", meanfunc, "in_file")
 
 """
-Generate linear registration matricies for each run to the anatomical
-volume and apply the registration to the timeseries(es)
+Generate linear registration matricies for each run to the structural image
 """
 
-reg2struct = pe.MapNode(interface=fs.BBRegister(contrast_type="bold",
-                                            init = "fsl"),
+reg2struct = pe.MapNode(interface=fs.BBRegister(contrast_type="t2",
+                                                init = "fsl"),
                         iterfield=["source_file"],
                         name="reg2struct")
 
 preproc.connect(meanfunc, "out_file", reg2struct, "source_file")
+preproc.connect(inputnode, "subject_id", reg2struct, "subject_id")
 
 """
-Get the brainmask volume from recon-all
+Apply this transformation to each mean image
 """
 
-masksource = pe.Node(interface=nio.FreeSurferSource(),
-                     name="masksource")
+meanxfm = pe.MapNode(interface=fs.ApplyVolTransform(fs_target=True,
+                                                    no_resample=True),
+                     iterfield=["reg_file","source_file"],
+                     name="applyxfm2mean")
+
+preproc.connect(meanfunc, "out_file", meanxfm, "source_file")
+preproc.connect(reg2struct, "out_reg_file", meanxfm, "reg_file")
 
 """
-Resample the brainmask image into native functional space
+Take a mean of the transformed mean images
 """
 
-resamplemask = pe.Node(interface=fs.ApplyVolTransform(inverse=True),
-                       name="resamplemask")
-
-preproc.connect([(masksource, resamplemask, [("brainmask", "source_file")]),
-                  (reg2struct, resamplemask, [("out_reg_file", "reg_file")])])
-
-"""
-Binarize the brainmask image in functional space to create a true mask
-"""
-
-binmask = pe.Node(interface=fs.Binarize(min=10),
-                  name="binarizemask")
-
-preproc.connect(resamplemask, "out_file", binmask, "in_file")
+meanmean = pe.Node(interface=fs.Concatenate(stats="mean"),
+                   name="meanmean")
+                   
+preproc.connect(meanxfm, "transformed_file", meanmean, "in_files")
 
 """
-Convert the mask image from mgz to nifti so it can be used by FSL tools
+Register this mean mean image to the structural image for surface transforms
 """
 
-niftimask = pe.Node(interface=fs.MRIConvert(out_type="niigz"),
-                    name="niftimask")
+surfreg = pe.Node(interface=fs.BBRegister(contrast_type="t2",
+                                          init = "fsl"),
+                  name="surfregister")
 
-preproc.connect(binmask, "out_file", niftimask, "in_file")
-
+preproc.connect(meanmean, "concatenated_file", surfreg, "source_file")
+preproc.connect(inputnode, "subject_id", surfreg, "subject_id")
 
 """
-Use this mask image to skullstrip each realigned functional run
+Strip the skull from the mean functional from each run
 """
 
-skullstrip = pe.MapNode(interface=fsl.ImageMaths(suffix="_strip",
-                                                 op_string="-mas"),
-                        iterfield=["in_file"],
-                        name="skullstrip")
+meanfuncmask = pe.MapNode(interface=fsl.BET(mask = True,
+                                         no_output=True,
+                                         frac = 0.3),
+                          iterfield = "in_file",
+                          name = "meanfuncmask")
+preproc.connect(meanfunc, "out_file", meanfuncmask, "in_file")
 
-preproc.connect([(motion_correct, skullstrip, [('out_file', 'in_file')]),
-                 (niftimask, skullstrip, [("out_file", "in_file2")])
-                 ]) 
+"""
+Mask the functional runs with the extracted mask
+"""
+
+maskfunc = pe.MapNode(interface=fsl.ImageMaths(suffix="_bet",
+                                               op_string="-mas"),
+                      iterfield=["in_file", "mask_file"],
+                      name = "maskfunc")
+preproc.connect(motion_correct, "out_file", maskfunc, "in_file")
+preproc.connect(meanfuncmask, "mask_file", maskfunc, "in_file2")
 
 """
 Determine the 2nd and 98th percentile intensities of each functional run
 """
 
-getthresh = pe.MapNode(interface=fsl.ImageStats(op_string='-p 2 -p 98'),
-                       iterfield = ['in_file'],
-                       name='getthreshold')
-preproc.connect(skullstrip, 'out_file', getthresh, 'in_file')
+getthresh = pe.MapNode(interface=fsl.ImageStats(op_string="-p 2 -p 98"),
+                       iterfield = ["in_file"],
+                       name="getthreshold")
+preproc.connect(maskfunc, "out_file", getthresh, "in_file")
 
 """
 Threshold the first run of the functional data at 10% of the 98th percentile
 """
 
-threshold = pe.Node(interface=fsl.ImageMaths(out_data_type='char',
-                                             suffix='_thresh'),
-                       name='threshold')
-preproc.connect(skullstrip, ('out_file', pickfirst), threshold, 'in_file')
-
+threshold = pe.MapNode(interface=fsl.ImageMaths(out_data_type="char",
+                                                suffix="_thresh"),
+                       iterfield = ["in_file"],
+                       name="threshold")
+preproc.connect(maskfunc, "out_file", threshold, "in_file")
 
 def getthreshop(thresh):
-    """Return 10% of the intensity as an fslmaths option string"""
-    return '-thr %.10f -Tmin -bin'%(0.1*thresh[0][1])
-preproc.connect(getthresh, ('out_stat', getthreshop), threshold, 'op_string')
+    """Return 10% of the intensity"""
+    return "-thr %.10f -Tmin -bin"%(0.1*thresh[0][1])
+preproc.connect(getthresh, ("out_stat", getthreshop), threshold, "op_string")
 
 """
 Determine the median value of the functional runs using the mask
 """
 
-medianval = pe.MapNode(interface=fsl.ImageStats(op_string='-k %s -p 50'),
-                       iterfield = ['in_file'],
-                       name='medianval')
-preproc.connect(motion_correct, 'out_file', medianval, 'in_file')
-preproc.connect(threshold, 'out_file', medianval, 'mask_file')
+medianval = pe.MapNode(interface=fsl.ImageStats(op_string="-k %s -p 50"),
+                       iterfield = ["in_file", "mask_file"],
+                       name="medianval")
+preproc.connect(motion_correct, "out_file", medianval, "in_file")
+preproc.connect(threshold, "out_file", medianval, "mask_file")
+
+"""
+Dilate the mask
+"""
+
+dilatemask = pe.MapNode(interface=fsl.ImageMaths(suffix="_dil",
+                                                 op_string="-dilF"),
+                        iterfield=["in_file"],
+                        name="dilatemask")
+preproc.connect(threshold, "out_file", dilatemask, "in_file")
+
+"""
+Mask the motion corrected functional runs with the dilated mask
+"""
+
+maskfunc2 = pe.MapNode(interface=fsl.ImageMaths(suffix="_mask",
+                                                op_string="-mas"),
+                      iterfield=["in_file", "in_file2"],
+                      name="maskfunc2")
+preproc.connect(motion_correct, "out_file", maskfunc2, "in_file")
+preproc.connect(dilatemask, "out_file", maskfunc2, "in_file2")
+
+"""
+Use RapidART to determine which of the
+images in the functional series are outliers based on deviations in
+intensity and/or movement.
+"""
+
+art = pe.Node(interface=ra.ArtifactDetect(use_differences = [True, False],
+                                          use_norm = True,
+                                          zintensity_threshold = 3,
+                                          norm_threshold = 1,
+                                          parameter_source = "FSL",
+                                          mask_type = "file"),
+              name="art")
+
+
+preproc.connect([(motion_correct, art, [("par_file","realignment_parameters")]),
+                 (maskfunc2, art, [("out_file","realigned_files")]),
+                 (dilatemask, art, [("out_file", "mask_file")]),
+                 ])
+                 
+"""
+Determine a new mean image from each functional run
+"""
+
+meanfunc2 = pe.MapNode(interface=fsl.ImageMaths(op_string="-Tmean",
+                                                suffix="_mean"),
+                       iterfield=["in_file"],
+                       name="meanfunc2")
+preproc.connect(maskfunc2, "out_file", meanfunc2, "in_file")
 
 """
 Merge the median values with the mean functional images into a coupled list
 """
 
-mergenode = pe.Node(interface=util.Merge(2, axis='hstack'),
-                    name='merge')
-preproc.connect(coregister, 'out_file', mergenode, 'in1')
-preproc.connect(medianval,'out_stat', mergenode, 'in2')
+mergenode = pe.Node(interface=util.Merge(2, axis="hstack"),
+                    name="merge")
+preproc.connect(meanfunc2,"out_file", mergenode, "in1")
+preproc.connect(medianval,"out_stat", mergenode, "in2")
 
+
+"""
+Identitynodes to set fwhm for smoothing
+"""
+volsmoothval = pe.Node(interface=util.IdentityInterface(fields=["fwhm"]),
+                       name="volsmoothval")
+surfsmoothval = pe.Node(interface=util.IdentityInterface(fields=["fwhm"]),
+                        name="surfsmoothval")
                        
 """
-Smooth each run using SUSAN with the brightness threshold set to 75% of the
-median value for each run and a mask consituting the mean functional
+Smooth each run in the volume using SUSAN with the brightness threshold set to
+75% of the median value for each run and a mask consituting the mean functional
 """
 
-smooth = pe.MapNode(interface=fsl.SUSAN(),
-                    iterfield=['in_file', 'brightness_threshold','usans'],
-                    name='smooth')
+volsmooth = pe.MapNode(interface=fsl.SUSAN(),
+                       iterfield=["in_file", "brightness_threshold", "usans"],
+                       name="volsmooth")
 
-"""
-Define a function to get the brightness threshold for SUSAN
-"""
 
 def getbtthresh(medianvals):
+    """Get the brightness threshold for SUSAN"""
     return [0.75*val for val in medianvals]
+preproc.connect(volsmoothval, "fwhm", volsmooth, "fwhm")
+preproc.connect(maskfunc2, "out_file", volsmooth, "in_file")
+preproc.connect(medianval, ("out_stat", getbtthresh), volsmooth, "brightness_threshold")
+preproc.connect(mergenode, 
+    ("out", lambda x: [[tuple([val[0],0.75*val[1]])] for val in x]), volsmooth, "usans")
 
-preproc.connect(applyxfm, 'out_file', smooth, 'in_file')
-preproc.connect(medianval, ('out_stat', getbtthresh), smooth, 'brightness_threshold')
-preproc.connect(mergenode, ('out', lambda x: [[tuple([val[0],0.75*val[1]])] for val in x]), smooth, 'usans')
+"""
+Use Freesurfer's vol/surf smoothing to smooth the images for surface analysis
+"""
+
+surfsmooth = pe.MapNode(interface=fs.Smooth(proj_frac_avg=(0,1,0.1)),
+                        iterfield=["in_file"],
+                        name="surfsmooth")
+
+preproc.connect(maskfunc2, "out_file", surfsmooth, "in_file")
+preproc.connect(surfsmoothval, "fwhm", surfsmooth, "surface_fwhm")
+preproc.connect(reg2struct, "out_reg_file", surfsmooth, "reg_file")
 
 """
 Mask the smoothed data with the dilated mask
 """
 
-maskfunc3 = pe.MapNode(interface=fsl.ImageMaths(suffix='_mask',
-                                                op_string='-mas'),
-                      iterfield=['in_file'],
-                      name='maskfunc3')
-preproc.connect(smooth, 'smoothed_file', maskfunc3, 'in_file')
-preproc.connect(dilatemask, 'out_file', maskfunc3, 'in_file2')
+volmaskfunc3 = pe.MapNode(interface=fsl.ImageMaths(suffix="_mask",
+                                                op_string="-mas"),
+                      iterfield=["in_file"],
+                      name="volmaskfunc3")
+preproc.connect(volsmooth, "smoothed_file", volmaskfunc3, "in_file")
+preproc.connect(dilatemask, "out_file", volmaskfunc3, "in_file2")
 
 """
-Scale each volume of the run so that the median value of the run is set to 10000
+Scale the median value of the run is set to 10000
 """
 
-intnorm = pe.MapNode(interface=fsl.ImageMaths(suffix='_intnorm'),
-                      iterfield=['in_file','op_string'],
-                      name='intnorm')
-preproc.connect(maskfunc3, 'out_file', intnorm, 'in_file')
+volmeanscale = pe.MapNode(interface=fsl.ImageMaths(suffix="_gms"),
+                          iterfield=["in_file","op_string"],
+                          name="volmeanscale")
+preproc.connect(volsmooth, "smoothed_file", volmeanscale, "in_file")
 
-"""
-Define a function to get the scaling factor for intensity normalization
-"""
+def getmeanscale(medianvals):
+    """Get the scaling factor for intensity normalization"""
+    return ["-mul %.10f"%(10000./val) for val in medianvals]
+preproc.connect(medianval, ("out_stat", getmeanscale), volmeanscale, "op_string")
 
-def getinormscale(medianvals):
-    return ['-mul %.10f'%(10000./val) for val in medianvals]
-preproc.connect(medianval, ('out_stat', getinormscale), intnorm, 'op_string')
+surfmeanscale = pe.MapNode(interface=fsl.ImageMaths(suffix="_gms"),
+                           iterfield=["in_file","op_string"],
+                           name="surfmeanscale")
+preproc.connect(surfsmooth, "smoothed_file", surfmeanscale, "op_string")
+preproc.connect(medianval, ("out_stat", getmeanscale), surfmeanscale, "op_string")
 
 """
 Perform temporal highpass filtering on the data
 """
 
-highpass = pe.MapNode(interface=fsl.ImageMaths(suffix='_tempfilt'),
-                      iterfield=['in_file'],
-                      name='highpass')
-preproc.connect(intnorm, 'out_file', highpass, 'in_file')
+volhighpass = pe.MapNode(interface=fsl.ImageMaths(suffix="_tempfilt"),
+                         iterfield=["in_file"],
+                         name="volhighpass")
+preproc.connect(volmeanscale, "out_file", volhighpass, "in_file")
 
-"""
-Convert the intensity normalization output to .nii so SPM can read it
-"""
+surfhighpass = pe.MapNode(interface=fsl.ImageMaths(suffix="_tempfilt"),
+                          iterfield=["in_file"],
+                          name="surfhighpass")
+preproc.connect(surfmeanscale, "out_file", surfhighpass, "in_file")
 
-convert = pe.MapNode(interface=fs.MRIConvert(out_type="nii"), 
-                     iterfield = "in_file",
-                     name="unzip_intnorm")
-preproc.connect(intnorm, "out_file", convert, "in_file")
-
-"""
-Generate a mean functional image from the first run
-"""
-
-meanfunc3 = pe.MapNode(interface=fsl.ImageMaths(op_string='-Tmean',
-                                                suffix='_mean'),
-                       iterfield=['in_file'],
-                      name='meanfunc3')
-preproc.connect(highpass, ('out_file', pickfirst), meanfunc3, 'in_file')
-
-
-"""
-Use rapidart to determine which of the images in the functional series are 
-outliers based on deviations in intensity and/or movement.
-"""
-
-art = pe.Node(interface=ra.ArtifactDetect(use_differences = [False,True],
-                                          use_norm = True,
-                                          norm_threshold = 0.5,
-                                          zintensity_threshold = 3,
-                                          parameter_source = 'FSL',
-                                          mask_type = 'file'),
-              name="art")
-
-
-preproc.connect([(motion_correct, art, [('par_file','realignment_parameters')]),
-                 (skullstrip, art, [('out_file','realigned_files')]),
-                 (niftimask, art, [('out_file', 'mask_file')]),
-                 ])
