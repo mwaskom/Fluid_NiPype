d450442864221f2ac5aa92c49fcb448751baaac6
Michael Waskom <mwaskom@mit.edu>
2010-10-13 12:09:14 -0400

    DARPA fixed effects module

--- fluid_fixed_fx.py
+++ fluid_fixed_fx.py
@@ -1,5 +1,6 @@
 import os
 import nipype.interfaces.io as nio
+import nipype.interfaces.freesurfer as fs
 import nipype.interfaces.fsl as fsl
 import nipype.algorithms.modelgen as model
 import nipype.interfaces.utility as util
@@ -19,11 +20,13 @@ Use :class:`nipype.interfaces.fsl.Merge` to merge the copes and
 varcopes for each condition
 """
 
-copemerge    = pe.Node(interface=fsl.Merge(dimension='t'),
+copemerge = pe.MapNode(interface=fsl.Merge(dimension='t'),
+                       iterfield=["in_files"],
                        name="copemerge")
 
-varcopemerge = pe.Node(interface=fsl.Merge(dimension='t'),
-                       name="varcopemerge")
+varcopemerge = pe.MapNode(interface=fsl.Merge(dimension='t'),
+                          iterfield=["in_files"],
+                          name="varcopemerge")
 
 """
 Use :class:`nipype.interfaces.fsl.L2Model` to generate subject and condition
@@ -37,15 +40,26 @@ level2model = pe.Node(interface=fsl.L2Model(),
 Use :class:`nipype.interfaces.fsl.FLAMEO` to estimate a second level model
 """
 
-flameo = pe.Node(interface=fsl.FLAMEO(run_mode='fe'), name="flameo")
+brain_mask = fsl.Info.standard_image("avg152T1_brain.nii.gz")
 
-overlayflame = pe.Node(interface=fsl.Overlay(stat_thresh=(2.5, 10),
+flamemask = pe.Node(fs.Binarize(min=10,in_file=brain_mask),
+                    name="flamemask")
+
+flameo = pe.MapNode(interface=fsl.FLAMEO(run_mode='fe'),
+                 iterfield=["cope_file","var_cope_file"],
+                 name="flameo")
+
+mni_brain = fsl.Info.standard_image("avg152T1.nii.gz")
+overlayflame = pe.MapNode(interface=fsl.Overlay(stat_thresh=(2.5, 10),
                                                 auto_thresh_bg=True,
-                                                show_negative_stats=True),
+                                                show_negative_stats=True,
+                                                background_image=mni_brain),
+                          iterfield=["stat_image"],
                           name='overlayflame')
 
-sliceflame = pe.Node(interface=fsl.Slicer(all_axial=True,
+sliceflame = pe.MapNode(interface=fsl.Slicer(all_axial=True,
                                              image_width=750),
+                        iterfield=["in_file"],
                         name='sliceflame')
 
 """
@@ -59,6 +73,7 @@ vol_fixed_fx.connect([(copemerge,flameo,[('merged_file','cope_file')]),
                       (level2model,flameo, [('design_mat','design_file'),
                                             ('design_con','t_con_file'),
                                             ('design_grp','cov_split_file')]),
+                      (flamemask,flameo, [("binary_file", "mask_file")]),
                       (flameo,overlayflame,[('zstats','stat_image')]),
                       (overlayflame,sliceflame,[('out_file', 'in_file')])
                   ])
