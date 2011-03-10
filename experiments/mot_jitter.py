b14b2e680252850e866bbe6ebfe693aee16a2ae7
Michael Waskom <mwaskom@mit.edu>
2011-03-10 11:50:32 -0500

    Updated MOT experiment files

--- experiments/mot_jitter.py
+++ experiments/mot_jitter.py
@@ -1,7 +1,6 @@
 """
     Nipype experiment module for the jittered MOT paradigm.
 """
-
 import re
 
 template_args = dict(timeseries=[["subject_id", "bold", ["MOT_Jitter_run?"]]])
@@ -15,28 +14,23 @@ units = "secs"
 nruns = 1
 
 fsl_bases = {"dgamma":{"derivs":False}}
-spm_bases = {"hrf":[0,0]}
 
-parfile_template = "%s/parfiles/MOT_Jitter_r%d_d1_%s_%s.txt"
+parfile_base_dir = "/mindhive/gablab/fluid/Data"
+parfile_template = "%s/parfiles/MOT_Jitter_r%d_d%d_%s_%s.txt"
+parfile_args = ["subject_id", "run_number", "day", "event", "subject_id"]
 
-names = ["speed1", "speed2", "speed3", "speed4", "resp"]
+events = ["speed1", "speed2", "speed3", "speed4", "resp"]
 
-cont01 = ["slow", "T", names, [1,0,0,0,0]]
-cont02 = ["normal", "T", names, [0,1,0,0,0]]
-cont03 = ["fast", "T", names, [0,0,1,0,0]]
-cont04 = ["vfast", "T", names, [0,0,0,1,0]]
-cont05 = ["slow-normal", "T", names, [1,-1,0,0,0]]
-cont06 = ["slow-fast", "T", names, [1,0,-1,0,0]]
-cont07 = ["slow-vfast", "T", names, [1,0,0,-1,0]]
-cont08 = ["normal-slow", "T", names, [-1,1,0,0,0]]
-cont09 = ["normal-fast", "T", names, [0,1,-1,0,0]]
-cont10 = ["normal-vfast", "T", names, [0,1,0,-1,0]]
-cont11 = ["fast-slow", "T", names, [-1,0,1,0,0]]
-cont12 = ["fast-normal", "T", names, [0,-1,1,0,0]]
-cont13 = ["fast-vfast", "T", names, [0,0,1,-1,0]]
-cont14 = ["vfast-slow", "T", names, [-1,0,0,1,0]]
-cont15 = ["vfast-normal", "T", names, [0,-1,0,1,0]]
-cont16 = ["vfast-fast", "T", names, [0,0,-1,1,0]]
+cont01 = ["slow",         "T", events, [1,0,0,0,0]]
+cont02 = ["normal",       "T", events, [0,1,0,0,0]]
+cont03 = ["fast",         "T", events, [0,0,1,0,0]]
+cont04 = ["vfast",        "T", events, [0,0,0,1,0]]
+cont05 = ["normal-slow",  "T", events, [-1,1,0,0,0]]
+cont06 = ["fast-slow",    "T", events, [-1,0,1,0,0]]
+cont07 = ["fast-normal",  "T", events, [0,-1,1,0,0]]
+cont08 = ["vfast-slow",   "T", events, [-1,0,0,1,0]]
+cont09 = ["vfast-normal", "T", events, [0,-1,0,1,0]]
+cont10 = ["vfast-fast",   "T", events, [0,0,-1,1,0]]
 
 convars = [var for var in dir() if re.match("cont\d+",var)]
 convars.sort()
